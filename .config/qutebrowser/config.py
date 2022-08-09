30ebbc7d39236b959d13d4d8e5f682d85da68a2e
wvovaw <vladimir5313375015@gmail.com>
2022-08-09 06:51:57 +0500

    OOOOOGAA BOOOGAAA

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -172,306 +172,341 @@ config.bind(',p', 'spawn --userscript qute-pass --dmenu-invocation dmenu')
 config.bind(',P', 'spawn --userscript qute-pass --dmenu-invocation dmenu --password-only')
 
 
-#c.qt.args = ['force-dark-mode', 'dark']
+c.qt.args = ['force-dark-mode', 'dark']
 #c.backend = 'webengine'
 # c.colors.webpage.prefers_color_scheme_dark=True
-#c.content.user_stylesheets = ['dark_theme.css']
+# c.content.user_stylesheets = ['dark_theme.css']
 
 
-# base16-qutebrowser (https://github.com/theova/base16-qutebrowser)
-# Base16 qutebrowser template by theova and Daniel Mulford
-# Solarized Dark scheme by Ethan Schoonover (modified by aramisgithub)
+# gruvbox dark hard qutebrowser theme by Florian Bruhin <me@the-compiler.org>
+#
+# Originally based on:
+#   base16-qutebrowser (https://github.com/theova/base16-qutebrowser)
+#   Base16 qutebrowser template by theova and Daniel Mulford
+#   Gruvbox dark, hard scheme by Dawid Kurek (dawikur@gmail.com), morhetz (https://github.com/morhetz/gruvbox)
 
-base00 = "#002b36"
-base01 = "#073642"
-base02 = "#586e75"
-base03 = "#657b83"
-base04 = "#839496"
-base05 = "#93a1a1"
-base06 = "#eee8d5"
-base07 = "#fdf6e3"
-base08 = "#dc322f"
-base09 = "#cb4b16"
-base0A = "#b58900"
-base0B = "#859900"
-base0C = "#2aa198"
-base0D = "#268bd2"
-base0E = "#6c71c4"
-base0F = "#d33682"
+bg0_hard = "#1d2021"
+bg0_soft = '#32302f'
+bg0_normal = '#282828'
 
-# set qutebrowser colors
+bg0 = bg0_normal
+bg1 = "#3c3836"
+bg2 = "#504945"
+bg3 = "#665c54"
+bg4 = "#7c6f64"
+
+fg0 = "#fbf1c7"
+fg1 = "#ebdbb2"
+fg2 = "#d5c4a1"
+fg3 = "#bdae93"
+fg4 = "#a89984"
+
+bright_red = "#fb4934"
+bright_green = "#b8bb26"
+bright_yellow = "#fabd2f"
+bright_blue = "#83a598"
+bright_purple = "#d3869b"
+bright_aqua = "#8ec07c"
+bright_gray = "#928374"
+bright_orange = "#fe8019"
+
+dark_red = "#cc241d"
+dark_green = "#98971a"
+dark_yellow = "#d79921"
+dark_blue = "#458588"
+dark_purple = "#b16286"
+dark_aqua = "#689d6a"
+dark_gray = "#a89984"
+dark_orange = "#d65d0e"
+
+### Completion
 
 # Text color of the completion widget. May be a single color to use for
 # all columns or a list of three colors, one for each column.
-c.colors.completion.fg = base05
+c.colors.completion.fg = [fg1, bright_aqua, bright_yellow]
 
 # Background color of the completion widget for odd rows.
-c.colors.completion.odd.bg = base00
+c.colors.completion.odd.bg = bg0
 
 # Background color of the completion widget for even rows.
-c.colors.completion.even.bg = base00
+c.colors.completion.even.bg = c.colors.completion.odd.bg
 
 # Foreground color of completion widget category headers.
-c.colors.completion.category.fg = base0D
+c.colors.completion.category.fg = bright_blue
 
 # Background color of the completion widget category headers.
-c.colors.completion.category.bg = base00
+c.colors.completion.category.bg = bg1
 
 # Top border color of the completion widget category headers.
-c.colors.completion.category.border.top = base00
+c.colors.completion.category.border.top = c.colors.completion.category.bg
 
 # Bottom border color of the completion widget category headers.
-c.colors.completion.category.border.bottom = base00
+c.colors.completion.category.border.bottom = c.colors.completion.category.bg
 
 # Foreground color of the selected completion item.
-c.colors.completion.item.selected.fg = base00
+c.colors.completion.item.selected.fg = fg0
 
 # Background color of the selected completion item.
-c.colors.completion.item.selected.bg = base0D
+c.colors.completion.item.selected.bg = bg4
 
-# Top border color of the selected completion item
-c.colors.completion.item.selected.border.top = base0D
+# Top border color of the selected completion item.
+c.colors.completion.item.selected.border.top = bg2
 
 # Bottom border color of the selected completion item.
-c.colors.completion.item.selected.border.bottom = base0D
+c.colors.completion.item.selected.border.bottom = c.colors.completion.item.selected.border.top
 
 # Foreground color of the matched text in the selected completion item.
-c.colors.completion.item.selected.match.fg = base00
+c.colors.completion.item.selected.match.fg = bright_orange
 
 # Foreground color of the matched text in the completion.
-c.colors.completion.match.fg = base09
+c.colors.completion.match.fg = c.colors.completion.item.selected.match.fg
 
 # Color of the scrollbar handle in the completion view.
-c.colors.completion.scrollbar.fg = base05
+c.colors.completion.scrollbar.fg = c.colors.completion.item.selected.fg
 
 # Color of the scrollbar in the completion view.
-c.colors.completion.scrollbar.bg = base00
+c.colors.completion.scrollbar.bg = c.colors.completion.category.bg
+
+### Context menu
 
 # Background color of disabled items in the context menu.
-c.colors.contextmenu.disabled.bg = base01
+c.colors.contextmenu.disabled.bg = bg3
 
 # Foreground color of disabled items in the context menu.
-c.colors.contextmenu.disabled.fg = base04
+c.colors.contextmenu.disabled.fg = fg3
 
 # Background color of the context menu. If set to null, the Qt default is used.
-c.colors.contextmenu.menu.bg = base00
+c.colors.contextmenu.menu.bg = bg0
 
 # Foreground color of the context menu. If set to null, the Qt default is used.
-c.colors.contextmenu.menu.fg =  base05
+c.colors.contextmenu.menu.fg =  fg2
 
 # Background color of the context menu’s selected item. If set to null, the Qt default is used.
-c.colors.contextmenu.selected.bg = base0D
+c.colors.contextmenu.selected.bg = bg2
 
 #Foreground color of the context menu’s selected item. If set to null, the Qt default is used.
-c.colors.contextmenu.selected.fg = base00
+c.colors.contextmenu.selected.fg = c.colors.contextmenu.menu.fg
+
+### Downloads
 
 # Background color for the download bar.
-c.colors.downloads.bar.bg = base00
+c.colors.downloads.bar.bg = bg0
 
 # Color gradient start for download text.
-c.colors.downloads.start.fg = base00
+c.colors.downloads.start.fg = bg0
 
 # Color gradient start for download backgrounds.
-c.colors.downloads.start.bg = base0D
+c.colors.downloads.start.bg = bright_blue
 
 # Color gradient end for download text.
-c.colors.downloads.stop.fg = base00
+c.colors.downloads.stop.fg = c.colors.downloads.start.fg
 
 # Color gradient stop for download backgrounds.
-c.colors.downloads.stop.bg = base0C
+c.colors.downloads.stop.bg = bright_aqua
 
 # Foreground color for downloads with errors.
-c.colors.downloads.error.fg = base08
+c.colors.downloads.error.fg = bright_red
+
+### Hints
 
 # Font color for hints.
-c.colors.hints.fg = base00
+c.colors.hints.fg = bg0
 
-# Background color for hints. Note that you can use a `rgba(...)` value
-# for transparency.
-c.colors.hints.bg = base0A
+# Background color for hints.
+c.colors.hints.bg = 'rgba(250, 191, 47, 200)'  # bright_yellow
 
 # Font color for the matched part of hints.
-c.colors.hints.match.fg = base05
+c.colors.hints.match.fg = bg4
+
+### Keyhint widget
 
 # Text color for the keyhint widget.
-c.colors.keyhint.fg = base05
+c.colors.keyhint.fg = fg4
 
 # Highlight color for keys to complete the current keychain.
-c.colors.keyhint.suffix.fg = base05
+c.colors.keyhint.suffix.fg = fg0
 
 # Background color of the keyhint widget.
-c.colors.keyhint.bg = base00
+c.colors.keyhint.bg = bg0
+
+### Messages
 
 # Foreground color of an error message.
-c.colors.messages.error.fg = base00
+c.colors.messages.error.fg = bg0
 
 # Background color of an error message.
-c.colors.messages.error.bg = base08
+c.colors.messages.error.bg = bright_red
 
 # Border color of an error message.
-c.colors.messages.error.border = base08
+c.colors.messages.error.border = c.colors.messages.error.bg
 
 # Foreground color of a warning message.
-c.colors.messages.warning.fg = base00
+c.colors.messages.warning.fg = bg0
 
 # Background color of a warning message.
-c.colors.messages.warning.bg = base0E
+c.colors.messages.warning.bg = bright_purple
 
 # Border color of a warning message.
-c.colors.messages.warning.border = base0E
+c.colors.messages.warning.border = c.colors.messages.warning.bg
 
 # Foreground color of an info message.
-c.colors.messages.info.fg = base05
+c.colors.messages.info.fg = fg2
 
 # Background color of an info message.
-c.colors.messages.info.bg = base00
+c.colors.messages.info.bg = bg0
 
 # Border color of an info message.
-c.colors.messages.info.border = base00
+c.colors.messages.info.border = c.colors.messages.info.bg
+
+### Prompts
 
 # Foreground color for prompts.
-c.colors.prompts.fg = base05
+c.colors.prompts.fg = fg2
 
 # Border used around UI elements in prompts.
-c.colors.prompts.border = base00
+c.colors.prompts.border = f'1px solid {bg1}'
 
 # Background color for prompts.
-c.colors.prompts.bg = base00
+c.colors.prompts.bg = bg3
 
 # Background color for the selected item in filename prompts.
-c.colors.prompts.selected.bg = base0A
+c.colors.prompts.selected.bg = bg2
+
+### Statusbar
 
 # Foreground color of the statusbar.
-c.colors.statusbar.normal.fg = base05
+c.colors.statusbar.normal.fg = fg2
 
 # Background color of the statusbar.
-c.colors.statusbar.normal.bg = base00
+c.colors.statusbar.normal.bg = bg0
 
 # Foreground color of the statusbar in insert mode.
-c.colors.statusbar.insert.fg = base0C
+c.colors.statusbar.insert.fg = bg0
 
 # Background color of the statusbar in insert mode.
-c.colors.statusbar.insert.bg = base00
+c.colors.statusbar.insert.bg = dark_aqua
 
 # Foreground color of the statusbar in passthrough mode.
-c.colors.statusbar.passthrough.fg = base0A
+c.colors.statusbar.passthrough.fg = bg0
 
 # Background color of the statusbar in passthrough mode.
-c.colors.statusbar.passthrough.bg = base00
+c.colors.statusbar.passthrough.bg = dark_blue
 
 # Foreground color of the statusbar in private browsing mode.
-c.colors.statusbar.private.fg = base0E
+c.colors.statusbar.private.fg = bright_purple
 
 # Background color of the statusbar in private browsing mode.
-c.colors.statusbar.private.bg = base00
+c.colors.statusbar.private.bg = bg0
 
 # Foreground color of the statusbar in command mode.
-c.colors.statusbar.command.fg = base04
+c.colors.statusbar.command.fg = fg3
 
 # Background color of the statusbar in command mode.
-c.colors.statusbar.command.bg = base01
+c.colors.statusbar.command.bg = bg1
 
 # Foreground color of the statusbar in private browsing + command mode.
-c.colors.statusbar.command.private.fg = base0E
+c.colors.statusbar.command.private.fg = c.colors.statusbar.private.fg
 
 # Background color of the statusbar in private browsing + command mode.
-c.colors.statusbar.command.private.bg = base01
+c.colors.statusbar.command.private.bg = c.colors.statusbar.command.bg
 
 # Foreground color of the statusbar in caret mode.
-c.colors.statusbar.caret.fg = base0D
+c.colors.statusbar.caret.fg = bg0
 
 # Background color of the statusbar in caret mode.
-c.colors.statusbar.caret.bg = base00
+c.colors.statusbar.caret.bg = dark_purple
 
 # Foreground color of the statusbar in caret mode with a selection.
-c.colors.statusbar.caret.selection.fg = base0D
+c.colors.statusbar.caret.selection.fg = c.colors.statusbar.caret.fg
 
 # Background color of the statusbar in caret mode with a selection.
-c.colors.statusbar.caret.selection.bg = base00
+c.colors.statusbar.caret.selection.bg = bright_purple
 
 # Background color of the progress bar.
-c.colors.statusbar.progress.bg = base0D
+c.colors.statusbar.progress.bg = bright_blue
 
 # Default foreground color of the URL in the statusbar.
-c.colors.statusbar.url.fg = base05
+c.colors.statusbar.url.fg = fg4
 
 # Foreground color of the URL in the statusbar on error.
-c.colors.statusbar.url.error.fg = base08
+c.colors.statusbar.url.error.fg = dark_red
 
 # Foreground color of the URL in the statusbar for hovered links.
-c.colors.statusbar.url.hover.fg = base09
+c.colors.statusbar.url.hover.fg = bright_orange
 
 # Foreground color of the URL in the statusbar on successful load
 # (http).
-c.colors.statusbar.url.success.http.fg = base0B
+c.colors.statusbar.url.success.http.fg = bright_red
 
 # Foreground color of the URL in the statusbar on successful load
 # (https).
-c.colors.statusbar.url.success.https.fg = base0B
+c.colors.statusbar.url.success.https.fg = fg0
 
 # Foreground color of the URL in the statusbar when there's a warning.
-c.colors.statusbar.url.warn.fg = base0E
+c.colors.statusbar.url.warn.fg = bright_purple
+
+### tabs
 
 # Background color of the tab bar.
-c.colors.tabs.bar.bg = base00
+c.colors.tabs.bar.bg = bg0
 
 # Color gradient start for the tab indicator.
-c.colors.tabs.indicator.start = base0D
+c.colors.tabs.indicator.start = bright_blue
 
 # Color gradient end for the tab indicator.
-c.colors.tabs.indicator.stop = base0C
+c.colors.tabs.indicator.stop = bright_aqua
 
 # Color for the tab indicator on errors.
-c.colors.tabs.indicator.error = base08
+c.colors.tabs.indicator.error = bright_red
 
 # Foreground color of unselected odd tabs.
-c.colors.tabs.odd.fg = base05
+c.colors.tabs.odd.fg = fg2
 
 # Background color of unselected odd tabs.
-c.colors.tabs.odd.bg = base00
+c.colors.tabs.odd.bg = bg2
 
 # Foreground color of unselected even tabs.
-c.colors.tabs.even.fg = base05
+c.colors.tabs.even.fg = c.colors.tabs.odd.fg
 
 # Background color of unselected even tabs.
-c.colors.tabs.even.bg = base00
-
-# Background color of pinned unselected even tabs.
-c.colors.tabs.pinned.even.bg = base0B
-
-# Foreground color of pinned unselected even tabs.
-c.colors.tabs.pinned.even.fg = base00
-
-# Background color of pinned unselected odd tabs.
-c.colors.tabs.pinned.odd.bg = base0B
-
-# Foreground color of pinned unselected odd tabs.
-c.colors.tabs.pinned.odd.fg = base00
-
-# Background color of pinned selected even tabs.
-c.colors.tabs.pinned.selected.even.bg = base0D
-
-# Foreground color of pinned selected even tabs.
-c.colors.tabs.pinned.selected.even.fg = base00
-
-# Background color of pinned selected odd tabs.
-c.colors.tabs.pinned.selected.odd.bg = base0D
-
-# Foreground color of pinned selected odd tabs.
-c.colors.tabs.pinned.selected.odd.fg = base00
+c.colors.tabs.even.bg = bg3
 
 # Foreground color of selected odd tabs.
-c.colors.tabs.selected.odd.fg = base00
+c.colors.tabs.selected.odd.fg = fg2
 
 # Background color of selected odd tabs.
-c.colors.tabs.selected.odd.bg = base0D
+c.colors.tabs.selected.odd.bg = bg0
 
 # Foreground color of selected even tabs.
-c.colors.tabs.selected.even.fg = base00
+c.colors.tabs.selected.even.fg = c.colors.tabs.selected.odd.fg
 
 # Background color of selected even tabs.
-c.colors.tabs.selected.even.bg = base0D
+c.colors.tabs.selected.even.bg = bg0
+
+# Background color of pinned unselected even tabs.
+c.colors.tabs.pinned.even.bg = bright_green
+
+# Foreground color of pinned unselected even tabs.
+c.colors.tabs.pinned.even.fg = bg2
+
+# Background color of pinned unselected odd tabs.
+c.colors.tabs.pinned.odd.bg = bright_green
+
+# Foreground color of pinned unselected odd tabs.
+c.colors.tabs.pinned.odd.fg = c.colors.tabs.pinned.even.fg
+
+# Background color of pinned selected even tabs.
+c.colors.tabs.pinned.selected.even.bg = bg0
+
+# Foreground color of pinned selected even tabs.
+c.colors.tabs.pinned.selected.even.fg = c.colors.tabs.selected.odd.fg
+
+# Background color of pinned selected odd tabs.
+c.colors.tabs.pinned.selected.odd.bg = c.colors.tabs.pinned.selected.even.bg
+
+# Foreground color of pinned selected odd tabs.
+c.colors.tabs.pinned.selected.odd.fg = c.colors.tabs.selected.odd.fg
 
 # Background color for webpages if unset (or empty to use the theme's
 # color).
-c.colors.webpage.bg = base00
+# c.colors.webpage.bg = bg4
--- .vimrc
+++ .vimrc
@@ -16,40 +16,43 @@ set rnu nu
 
 call vundle#begin()
 Plugin 'gmarik/Vundle.vim' "!!!Vundle plugin manager!!!
+
 Plugin 'junegunn/fzf', {'do': { -> fzf#install() } } " FZF
 Plugin 'scrooloose/nerdtree' " file drawer, open with :NERDTreeToggle
 Plugin 'Xuyuanp/nerdtree-git-plugin' " Add git interraction to neardtree
 Plugin 'tiagofumo/vim-nerdtree-syntax-highlight' " Highlight nerdtree
-" Plugin 'benmills/vimux' "Tmux integration. Try Vimux in command mode
 Plugin 'tpope/vim-fugitive' "Ultimate git helper
+Plugin 'airblade/vim-gitgutter' " Show line changes 
+Plugin 'jiangmiao/auto-pairs' " Braces control
 Plugin 'tpope/vim-commentary' "Comment/uncomment lines with gcc or gc in visual mode
-Plugin 'dracula/vim', { 'name': 'dracula' } "Dracula theme 
-Plugin 'arcticicestudio/nord-vim' " Nord theme
+Plugin 'machakann/vim-highlightedyank' " Highlight yunked text
+Plugin 'chrisbra/colorizer' " Colorize hex colors
+Plugin 'sheerun/vim-polyglot' "Syntax highlighting for almost everything
+Plugin 'neoclide/coc.nvim', {'branch': 'release'} " Vim becomves smarter
+Plugin 'editorconfig/editorconfig-vim'
+Plugin 'posva/vim-vue' " VUE js 
+
+" Plugin 'dracula/vim', { 'name': 'dracula' } "Dracula theme 
+" Plugin 'arcticicestudio/nord-vim' " Nord theme
 Plugin 'altercation/vim-colors-solarized' " Solarized
 Plugin 'morhetz/gruvbox' " Gruuuuuuvboooox
 Plugin 'tomasiser/vim-code-dark' " Code colorscheme
 Plugin 'ryanoasis/vim-devicons' "NerdFont for vim and NerdTree
 Plugin 'vim-airline/vim-airline' "Statusline mod
 Plugin 'vim-airline/vim-airline-themes' "And themes
-"Plugin 'Valloric/YouCompleteMe' "Code completer for many languages
-Plugin 'vim-python/python-syntax' "Python syntax highlighting
-Plugin 'sheerun/vim-polyglot' "Syntax highlighting for almost everything
-Plugin 'neoclide/coc.nvim', {'branch': 'release'} " Vim becomves smarter
-Plugin 'airblade/vim-gitgutter' " Show line changes 
-Plugin 'mhinz/vim-startify' " Vim start winow
-Plugin 'machakann/vim-highlightedyank' " Highlight yunked text
-Plugin 'wsdjeg/vim-todo' " Todo manager BROKEN (Fixed)
-Plugin 'chrisbra/colorizer' " Colorize hex colors
-Plugin 'fatih/vim-go' " Go utils
-Plugin 'editorconfig/editorconfig-vim'
+
+" Plugin 'benmills/vimux' "Tmux integration. Try Vimux in command mode
+" Plugin 'Valloric/YouCompleteMe' "Code completer for many languages
+" Plugin 'vim-python/python-syntax' "Python syntax highlighting
+" Plugin 'mhinz/vim-startify' " Vim start winow
+" Plugin 'wsdjeg/vim-todo' " Todo manager BROKEN (Fixed)
+" Plugin 'fatih/vim-go' " Go utils
 " Plugin 'Raimondi/delimitMate' " Doesn't work
-Plugin 'jiangmiao/auto-pairs' " Braces control
-Plugin 'posva/vim-vue' " VUE js 
 call vundle#end()
 
 """Includes
 so ~/.cocrc.vim
-so ~/.startifyrc.vim
+" so ~/.startifyrc.vim
 """
 
 """Mappings
@@ -71,7 +74,6 @@ map <C-n> :NERDTreeToggle<CR>
 """FZF stuf
 map <C-p> :FZF<CR> 
 """
-
 """Commands
 command! OneBuf execute '%bd|edit #|normal `"'
 au BufNewFile,BufRead *.ejs set filetype=html
@@ -85,27 +87,36 @@ let g:highlightedyank_highlight_duration = 1000
 " colorscheme dracula 
 " colorscheme solarized
 " colorscheme nord
-colorscheme codedark
+" colorscheme codedark
 
-" colorscheme gruvbox
-" let g:gruvbox_contrast_dark = 'hard'
-" let g:gruvbox_italic = '1'
-" let g:italicize_strings = '1'
+colorscheme gruvbox
+let g:gruvbox_contrast_dark = 'hard'
+let g:gruvbox_italic = '1'
+let g:italicize_strings = '1'
 "
 
 """Airline stuf
 "Theme
 " let g:airline_theme = 'dracula'
-" let g:airline_theme = 'gruvbox'
-let g:airline_theme = 'codedark'
+let g:airline_theme = 'minimalist'
+" let g:airline_theme = 'codedark'
 " let g:airline_theme = 'solarized'
 "Enable tabline
 let g:airline#extensions#tabline#enabled = 1
+
+if !exists('g:airline_symbols')
+  let g:airline_symbols = {}
+endif
+
 let g:airline#extensions#tabline#left_sep = "\uE0B4"
 let g:airline#extensions#tabline#left_alt_sep = "\uE0B5"
 let g:airline#extensions#tabline#right_sep = "\uE0B6"
 let g:airline#extensions#tabline#right_alt_sep = "\uE0B7"
-
+let g:airline_symbols.branch = ' '
+let g:airline_symbols.readonly = ''
+let g:airline_symbols.linenr = '☰ '
+let g:airline_symbols.maxlinenr = ''
+let g:airline_symbols.dirty='⚡'
 "Enable powerline fonts
 let g:airline_powerline_fonts = 1
 let g:airline_left_sep = "\uE0B4"
@@ -113,5 +124,8 @@ let g:airline_right_sep = "\uE0B6"
 let g:webdevicons_enable_airline_statusline = 1 " Enable to statusline
 let g:webdevicons_enable_ctrlp = 1  " Enable for CtrlP
 " :echo WebDevIconsGetFileTypeSymbol(expand('%:t'))
-let g:airline_section_x = '%{airline#util#wrap(airline#parts#filetype(),1)}%{" "}%{WebDevIconsGetFileTypeSymbol()}' " Add glyph after filetype
+let g:airline_section_c = '%f%{" "}%{WebDevIconsGetFileTypeSymbol()}' " Add glyph after filetype
+let g:airline_section_x = '%p%% %{g:airline_symbols.linenr}%l/%L'
+let g:airline_section_y = ''
+let g:airline_section_z = ''
 """
